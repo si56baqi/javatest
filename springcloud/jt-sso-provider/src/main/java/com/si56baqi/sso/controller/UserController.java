fc9f3d6ca6b1b4749149516bdc58fe8e37001c15
si56baqi <si56baqi@163.com>
2020-09-01 16:36:00 +0800

    sso搭建

--- springcloud/jt-sso-provider/src/main/java/com/si56baqi/sso/controller/UserController.java
+++ springcloud/jt-sso-provider/src/main/java/com/si56baqi/sso/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.si56baqi.sso.controller;
 
+import com.si56baqi.common.vo.SysResult;
+import com.si56baqi.sso.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 检测数据是否可用
+     *
+     * @param param 用户输入的参数
+     * @param type type为类型，可选参数1、2、3分别代表username、phone、email
+     * @return
+     */
+    @RequestMapping(value = "/check/{param}/{type}", method = RequestMethod.GET)
+    @ResponseBody
+    public SysResult check(@PathVariable("param") String param, @PathVariable("type") Integer type) {
+        return userService.check(param, type);
+    }
+
 }
--- springcloud/jt-sso-provider/src/main/java/com/si56baqi/sso/service/impl/UserServiceImpl.java
+++ springcloud/jt-sso-provider/src/main/java/com/si56baqi/sso/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.si56baqi.sso.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.si56baqi.common.service.RedisService;
 import com.si56baqi.common.vo.SysResult;
 import com.si56baqi.sso.mapper.UserMapper;
@@ -8,15 +9,35 @@ import com.si56baqi.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
     @Autowired
     private RedisService redisService;
+    //把java对象转成json串，ObjectMapper
+    private static final ObjectMapper mapper=new ObjectMapper();
+    //根据type=1:username，2:phone，3:email
+    private static final Map<Integer, String> PARAM_TYPE = new HashMap<Integer, String>();
+    //初始化
+
+    static {
+        PARAM_TYPE.put(1, "username");
+        PARAM_TYPE.put(2, "phone");
+        PARAM_TYPE.put(3, "email");
+    }
+
     @Override
     public SysResult check(String param, Integer type) {
-        return null;
+        int i=userMapper.check(PARAM_TYPE.get(type), param);
+        if(i==0) {
+            return SysResult.ok("false");	//数据库没有，可用
+        }else {
+            return SysResult.build(201, "OK", "true");	//数据库值已经存在，不可用
+        }
     }
 
     @Override
