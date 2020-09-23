fd42e0db69de7bc6bef5d90a76a608a664bfb77c
si56baqi <si56baqi@163.com>
2020-09-23 17:18:58 +0800

    java基础

---
+++ springcloud/javapor-base/src/fristday/JavaObject/Cycle.java
@@ -0,0 +1,27 @@
+package fristday.JavaObject;
+//循环
+public class Cycle {
+    public static void main(String[] args) {
+       /* int x=10;
+        while (x<20){
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+        do {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }while (x<20);*/
+
+       /* for( x = 10; x < 20; x = x+1) {
+            System.out.print("value of x : " + x );
+            System.out.print("\n");
+        }*/
+       int [] a={1,2,3,4,4,5};
+        for (int b:a
+             ) {
+            System.out.println(b);
+        }
+    }
+}
--- springcloud/javapor-base/src/fristday/JavaObject/Test.java
+++ springcloud/javapor-base/src/fristday/JavaObject/Test.java
@@ -11,6 +11,50 @@ public class Test {
     public static void main(String[] args){
         Test test = new Test();
         test.pupAge();
+        int a = 10;
+        int b = 20;
+        int c =0;
+        /*int d = 25;
+        System.out.println("a + b = " + (a + b) );//30
+        System.out.println("a - b = " + (a - b) );//-10
+        System.out.println("a * b = " + (a * b) );
+        System.out.println("b / a = " + (b / a) );
+        System.out.println("b % a = " + (b % a) );
+        System.out.println("c % a = " + (c % a) );
+        System.out.println("a++   = " +  (a++) );
+        System.out.println("a--   = " +  (a--) );
+        // 查看  d++ 与 ++d 的不同
+        System.out.println("d++   = " +  (d++) );
+        System.out.println("++d   = " +  (++d) );*/
+
+        c = a + b;
+        System.out.println("c = a + b = " + c );//30
+        c += a ;//c=c+a
+        System.out.println("c += a  = " + c );//40
+        c -= a ;//c=c-a 40-10
+        System.out.println("c -= a = " + c );//30
+        c *= a ;//c=c*a 30*10
+        System.out.println("c *= a = " + c );//300
+        a = 10;
+        c = 15;
+        c /= a ;//c=c/a
+        System.out.println("c /= a = " + c );
+        a = 10;
+        c = 15;
+        c %= a ;//c=c%a
+        System.out.println("c %= a  = " + c );
+        c <<= 2 ;
+        System.out.println("c <<= 2 = " + c );
+        c >>= 2 ;
+        System.out.println("c >>= 2 = " + c );
+        c >>= 2 ;
+        System.out.println("c >>= 2 = " + c );
+        c &= a ;
+        System.out.println("c &= a  = " + c );
+        c ^= a ;
+        System.out.println("c ^= a   = " + c );
+        c |= a ;
+        System.out.println("c |= a   = " + c );
     }
 }
 /*
