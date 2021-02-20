77e2a7c8e410bad1e433cd540e8ee12640e5e02a
si56baqi <si56baqi@163.com>
2021-02-20 17:05:17 +0800

    c3p0练习

--- spring/comsi56baqi/src/main/java/jdbcMysqlTest/C3p0Select.java
+++ spring/comsi56baqi/src/main/java/jdbcMysqlTest/C3p0Select.java
@@ -1,8 +1,30 @@
 package jdbcMysqlTest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 public class C3p0Select{
-    public static void main(String[] args) throws SQLException{
-        System.out.println(111111111);
+    public static void main(String[] args){
+        Connection connection=null;
+        PreparedStatement preparedStatement=null;
+        try {
+            connection=DataSourceUtils.getConnection();
+            String sql="insert into student(id,name,age) values(?,?,?)";
+            preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setInt(1,33);
+            preparedStatement.setString(2,"开心");
+            preparedStatement.setInt(3,5);
+            int rows=preparedStatement.executeUpdate();
+            if (rows>0){
+                System.out.println("添加成功");
+            }else {
+                System.out.println("添加失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DataSourceUtils.CloseResource(connection,preparedStatement,null);
+        }
+
     }
 }
